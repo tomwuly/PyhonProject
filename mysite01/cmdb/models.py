21981fc3c82a1210bc459c054665e8495247b856
tomwuly <qqwulingyun@163.com>
2017-09-04 11:09:45 +0800

    添加数据库之前
    Signed-off-by: tomwuly <qqwulingyun@163.com>

--- mysite01/cmdb/models.py
+++ mysite01/cmdb/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class user_info(models.Model):
+    user = models.CharField(max_length=32)
+    pwd = models.CharField(max_length=32)
--- mysite01/cmdb/views.py
+++ mysite01/cmdb/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+
+user_list=[
+    {"user":"jack","pwd":"abc"},
+    {"user":"tom","pwd":"ABC"},
+]
 def index(request):
-    return render(request,"index.html")
+    #return render(request,"index.html")
+    if request.method == "POST":
+        username = request.POST.get("username",None)
+        password = request.POST.get("password",None)
+        #print(username,password)
+        temp = {"user":username,"pwd":password}
+        user_list.append(temp)
+    return render(request,"index.html",{"data":user_list})
