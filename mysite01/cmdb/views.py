74a7dcb0ef21dd1d2028ea7ee9d93f8e10e950a7
tomwuly <qqwulingyun@163.com>
2017-09-04 11:29:11 +0800

    提交显示数据成功
    Signed-off-by: tomwuly <qqwulingyun@163.com>

--- mysite01/cmdb/views.py
+++ mysite01/cmdb/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from cmdb import models
 # Create your views here.
 
-user_list=[
-    {"user":"jack","pwd":"abc"},
-    {"user":"tom","pwd":"ABC"},
-]
+# user_list=[
+#     {"user":"jack","pwd":"abc"},
+#     {"user":"tom","pwd":"ABC"},
+# ]
 def index(request):
     #return render(request,"index.html")
     if request.method == "POST":
         username = request.POST.get("username",None)
         password = request.POST.get("password",None)
         #print(username,password)
-        temp = {"user":username,"pwd":password}
-        user_list.append(temp)
+        # temp = {"user":username,"pwd":password}
+        # user_list.append(temp)
+        models.user_info.objects.create(user=username,pwd=password)
+    user_list = models.user_info.objects.all()
     return render(request,"index.html",{"data":user_list})
